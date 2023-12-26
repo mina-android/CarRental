e559fe0715e1ee48eac099df34eeb9e1588eb2c1
MinaAndroid <104459927+mina-android@users.noreply.github.com>
2023-12-26 12:18:35 +0200

    Add files via upload

---
+++ src/carsrental/CarsRental.java
@@ -0,0 +1,15 @@
+package carsrental;
+public class CarsRental {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HomeGUI HomeFtame = new HomeGUI(); 
+        HomeFtame.setVisible(true);
+        HomeFtame.pack();
+        HomeFtame.setLocationRelativeTo(null);
+        
+        
+        
+        
+    }
+    
+}
